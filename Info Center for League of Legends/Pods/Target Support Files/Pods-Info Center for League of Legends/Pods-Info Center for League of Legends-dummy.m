3c0f670fc46e921681dd73ddf3be54dac5ab8cd4
Victor Ilisei <vilisei@gmail.com>
2017-04-15 17:29:21 -0700

    Added Firebase pods

--- Info Center for League of Legends/Pods/Target Support Files/Pods-Info Center for League of Legends/Pods-Info Center for League of Legends-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Info_Center_for_League_of_Legends : NSObject
-@end
-@implementation PodsDummy_Pods_Info_Center_for_League_of_Legends
-@end
